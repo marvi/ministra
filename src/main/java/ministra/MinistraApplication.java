e251c6ab13682b6c77fa2d9443698550e55bd6d5
Markus Härnvi <markus@harnvi.net>
2026-09-08 22:42:33 +0200

    Merge

---
+++ src/main/java/ministra/MinistraApplication.java
@@ -0,0 +1,13 @@
+package ministra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinistraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinistraApplication.class, args);
+	}
+
+}
